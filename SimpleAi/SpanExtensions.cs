910d13b6fe5e89327c4df9be427fdd26507f7017
GGG <gggkiller2@gmail.com>
2025-02-19 03:55:06 -0300

    Add debug checks for UnsafeIndex.

--- SimpleAi/SpanExtensions.cs
+++ SimpleAi/SpanExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace SimpleAi;
@@ -11,9 +12,30 @@ internal static class SpanExtensions
 
     public static ref T Ref<T>(this ReadOnlySpan<T> span) => ref MemoryMarshal.GetReference(span);
 
-    public static ref T UnsafeIndex<T>(this T[] array, int offset) => ref Unsafe.Add(ref array.Ref(), offset);
+    public static ref T UnsafeIndex<T>(this T[] array, int offset)
+    {
+        Debug.Assert(array.Length >= offset, "Attempt to do out of bounds indexing.");
+        return ref Unsafe.Add(ref array.Ref(), offset);
+    }
 
-    public static ref T UnsafeIndex<T>(this Span<T> span, int offset) => ref Unsafe.Add(ref span.Ref(), offset);
+    public static ref T UnsafeIndex<T>(this T[] array, Index index)
+        => ref array.UnsafeIndex(index.GetOffset(array.Length));
 
-    public static ref T UnsafeIndex<T>(this ReadOnlySpan<T> span, int offset) => ref Unsafe.Add(ref span.Ref(), offset);
+    public static ref T UnsafeIndex<T>(this Span<T> span, int offset)
+    {
+        Debug.Assert(span.Length >= offset, "Attempt to do out of bounds indexing.");
+        return ref Unsafe.Add(ref span.Ref(), offset);
+    }
+
+    public static ref T UnsafeIndex<T>(this Span<T> span, Index index)
+        => ref span.UnsafeIndex(index.GetOffset(span.Length));
+
+    public static ref T UnsafeIndex<T>(this ReadOnlySpan<T> span, int offset)
+    {
+        Debug.Assert(span.Length >= offset, "Attempt to do out of bounds indexing.");
+        return ref Unsafe.Add(ref span.Ref(), offset);
+    }
+
+    public static ref T UnsafeIndex<T>(this ReadOnlySpan<T> span, Index index)
+        => ref span.UnsafeIndex(index.GetOffset(span.Length));
 }
